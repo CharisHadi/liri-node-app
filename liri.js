41e66281ee23b6c0901c26bd7700beb8a5c1a59e
CharisHadi <charishadi@gmail.com>
2019-06-13 21:30:26 -0700

    edit key stuff

--- liri.js
+++ liri.js
@@ -2,3 +2,6 @@ require("dotenv").config();
 var axios = require("axios");
 var fs = require("fs");
 var inquirer = require("inquirer");
+
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
