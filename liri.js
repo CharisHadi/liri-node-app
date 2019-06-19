eb00407c6ed301254a2c88f8a83fdfb23288b1c9
CharisHadi <charishadi@gmail.com>
2019-06-18 19:25:22 -0700

    naming conventions fixed, user input stored in variable

--- liri.js
+++ liri.js
@@ -1,7 +1,26 @@
 require("dotenv").config();
+
+
 var axios = require("axios");
 var fs = require("fs");
-var inquirer = require("inquirer");
-
+var Spotify = require("node-spotify-api")
 var keys = require("./keys.js");
+
 var spotify = new Spotify(keys.spotify);
+
+var command = process.argv[2];
+console.log(command);
+
+switch (command){
+    case "concert-this":
+        
+        break;
+    case "spotify-this":
+        
+        break;
+
+    case "movie-this":
+        
+        break;
+
+}
