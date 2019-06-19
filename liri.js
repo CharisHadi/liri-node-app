16352895940c82a55d2d25c5c2d380b04c22e0be
CharisHadi <charishadi@gmail.com>
2019-06-18 19:36:59 -0700

    added comments, takes in user input for query

--- liri.js
+++ liri.js
@@ -1,15 +1,17 @@
+//Package Requring for proper app functionality
 require("dotenv").config();
-
-
 var axios = require("axios");
 var fs = require("fs");
 var Spotify = require("node-spotify-api")
 var keys = require("./keys.js");
 
+//initializng spotify API with keys
 var spotify = new Spotify(keys.spotify);
 
+//Store User input into variables for command and query
 var command = process.argv[2];
-console.log(command);
+var searchInput = process.argv[3];
+// console.log(searchInput);
 
 switch (command){
     case "concert-this":
