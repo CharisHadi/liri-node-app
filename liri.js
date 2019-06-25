f6984712756ded13e232ce09363587820d91fc26
CharisHadi <charishadi@gmail.com>
2019-06-25 10:54:55 -0700

    Bands-in-town and Spotify API Implimentation
    -got em done

--- liri.js
+++ liri.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 var axios = require("axios");
 var fs = require("fs");
 var Spotify = require("node-spotify-api")
+var moment = require("moment");
 var keys = require("./keys.js");
 
 //initializng spotify API with keys
@@ -15,14 +16,47 @@ var searchInput = process.argv[3];
 
 switch (command){
     case "concert-this":
-        
+        axios.get("https://rest.bandsintown.com/artists/" + searchInput + "/events?app_id=codingbootcamp")
+        .then(function (response) {
+            var data = response.data;
+            //console.log(data[0]);
+            for(var i = 0; i < data.length; i++){
+                console.log("Venue: " + data[i].venue.name);
+                console.log("Location: " + data[i].venue.city + ", " + data[i].venue.country);
+                console.log("Time: " + data[i].datetime);
+                console.log("------------------------------------------------------");
+                //console.log(moment().format(data[i].datetime, "MM-DD-YYYY"));
+
+            }
+        })
+        .catch(function (error) {
+            // handle error
+            console.log("error occurred: " + error);
+        });
         break;
-    case "spotify-this":
-        
+    case "spotify-this-song":
+        spotify
+        .search({type: 'track', query: searchInput, limit: 5})
+        .then(function(response) {
+            var data = response.tracks.items;
+
+            for(var i = 0; i < data.length; i++){
+                console.log("Artist: " + data[i].artists[0].name);
+                console.log("Track Title: " + data[i].name);
+                console.log("Album Title: " + data[i].album.name);
+                console.log("URL to Song: " + data[i].artists[0].external_urls.spotify);
+                console.log("------------------------------------------------------")
+            }
+        })
+        .catch(function (error) {
+            console.log(error)
+        });
         break;
 
     case "movie-this":
         
+        break;
+    case "do-what-it-says":
         break;
 
 }
